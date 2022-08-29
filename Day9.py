2eb51d7c3c127ebf053022796551c056d2b9eac3
YuraYeghiazaryan <101919335+YuraYeghiazaryan@users.noreply.github.com>
2022-08-29 13:49:49 +0400

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -1,1 +1,17 @@
+#Task2===============================
 
+import os
+import random
+with open("file","w") as f:
+    while os.stat("file").st_size < 4 * 10 ** 6:
+        f.write(f"{random.randint(1, 200
+
+
+#Task3===============================
+
+dic = {}
+with open ("file") as f:
+    for i in f:
+        dic[i] = dic.get(i, 0) + 1
+    for x in sorted(dic):
+        f.write(f"{(x + '') * dic[x]}")
